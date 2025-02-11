f8c37a2b98fa07c5e6b95c92a6f0d682f63ac9af
Rafael Castro <rafael.actt@gmail.com>
2025-02-11 14:12:06 -0300

    Add project files.

---
+++ TesteEasy/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TesteEasy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Easy Test!");
+            Console.WriteLine("Resultado "+Solution.DetectCapitalUse("USA"));
+            Console.WriteLine("Resultado "+Solution.DetectCapitalUse("ResultadO"));
+            Console.WriteLine("Resultado "+Solution.DetectCapitalUse("Rafael"));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ TesteEasy/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEasy
+{
+    public static class Solution
+    {
+        public static bool DetectCapitalUse(string word)
+        {
+            if (word.Length <= 1) return true;
+            bool wordUpper = false;
+            bool wordLower = false;
+            bool wordLowerMiddle = false;
+
+            if (Char.IsLower(word[0]))
+            {
+                wordLower = true;
+            }//end if
+            else if (Char.IsUpper(word[0]) && Char.IsLower(word[word.Length - 1]))
+            {
+                wordLowerMiddle = true;
+            }
+            else
+                wordUpper = true;
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                if ((wordLowerMiddle || wordLower) && Char.IsUpper(word[i])) return false;
+
+                if (wordUpper && Char.IsLower(word[i])) return false;
+            }
+
+            return true;
+        }//end DetectCapitalUse
+    }
+
+
+}
